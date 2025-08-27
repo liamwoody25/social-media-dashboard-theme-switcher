42261732aef45d3b3a248c4b035887aada80dba8
liam woodward <liamwoodward20@outlook.com>
2025-08-27 11:11:29 +0100

    adding to javascript

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,26 @@
 const themeToggle = document.querySelector('.darkmode-toggle');
-let body = document.getElementsByTagName('body')[0]
+let body = document.getElementsByTagName('body')[0];
+let data = document.querySelector('.card');
+const dataContent = document.querySelector('.stat-card')
 
-
-themeToggle.addEventListener('click', function(){
-  
-  if (themeToggle.classList.toggle('active')) {
+function darkMode() {
+   if (themeToggle.classList.toggle('active')) {
     document.body.style.backgroundColor = '#252B42'
+    data.style.backgroundColor = '#252B42'
+    data. 
   } else {
     document.body.style.backgroundColor = '#fff'
   }
+}
+
+function lightMode() {
+  if (themeToggle.classList.toggle('active')) {
+    document.body.style.backgroundColor = '#fff'
+  }
+}
+
+themeToggle.addEventListener('click', function(){
+ darkMode()
+ lightMode()
+ 
 })
