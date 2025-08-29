7aa8fa24833ea2c6f9e49db8eb2d9e70b76af69d
liam woodward <liamwoodward20@outlook.com>
2025-08-29 10:55:54 +0100

    making changes to javascript

--- public/js/script.js
+++ public/js/script.js
@@ -1,26 +1,53 @@
 const themeToggle = document.querySelector('.darkmode-toggle');
-let body = document.getElementsByTagName('body')[0];
-let data = document.querySelector('.card');
-const dataContent = document.querySelector('.stat-card')
+let body = document.getElementsByTagName('body');
+let header = document.querySelector('header');
+const data = document.querySelector('.card');
+const headerHd = document.getElementsByTagName('h3');
+const cardHd = document.querySelector('h1'); 
+const dataContent = document.querySelector('.data-info');
+const headerTxt = document.getElementsByTagName('p');
 
-function darkMode() {
-   if (themeToggle.classList.toggle('active')) {
-    document.body.style.backgroundColor = '#252B42'
-    data.style.backgroundColor = '#252B42'
-    data. 
-  } else {
-    document.body.style.backgroundColor = '#fff'
-  }
-}
 
-function lightMode() {
+// this function is for when the toggle is clicked 
+function themeSwitcher() {
+
   if (themeToggle.classList.toggle('active')) {
+
+   for (let i = 0; i < headerHd.length; i++) {
+    headerHd[i].style.color = '#fff'
+    headerTxt[i].style.color = '#AEB3CB'
+   }
+    document.body.style.backgroundColor = '#252B42'
+    header.style.backgroundColor = '#20222F'
+    data.style.backgroundColor = '#252B42'
+    dataContent.style.color = '#fff'
+  } else {
+      for (let i = 0; i < headerHd.length; i++) {
+        headerHd[i].style.color = '#1D1F29'
+        headerTxt[i].style.color = '#63687D'
+      }
     document.body.style.backgroundColor = '#fff'
+    header.style.backgroundColor = '#f5f7ff'
+    data.style.backgroundColor = '#f0f2fa'
+    cardHd.style.color = '#1D1F29'
   }
 }
+
 
 themeToggle.addEventListener('click', function(){
- darkMode()
- lightMode()
- 
+  themeSwitcher()
 })
+
+
+
+  //  if (themeToggle.classList.toggle('active')) {
+  //   document.body.style.backgroundColor = '#252B42';
+  //   header.style.backgroundColor = '#20222F';
+  //   headerTxt.style.color = '#fff'
+  //   data.style.backgroundColor = '#252B42'
+  //   dataContent.style.color = '#ffff'
+  // } else {
+  //   data.style.backgroundColor = '#f0f2fa'
+  //   header.style.backgroundColor = '#f5f7ff'
+  //   headerTxt.style.color = '#1D1F29'
+  // }
